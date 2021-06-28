4d37b114c4c9bd7e12f1daadb228a4a10294da49
MkDierz <msyibbran24@gmail.com>
2021-06-29 02:30:57 +0700

    Update bawah.php

--- layouts/bawah.php
+++ layouts/bawah.php
@@ -1,14 +1,31 @@
-</div>
-</div>
-</div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.
-min.js" integrity="sha384-
-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min
-.js" integrity="sha384-
-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+            </div>
+        </div>
+    </div>
+
+
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+		
+	<!--Datatables -->
+	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script>
+		$(document).ready(function() {
+			
+			var table = $('#example').DataTable( {
+					responsive: true,
+					"paging":   false,
+					"searching":false,
+					"sort":     false
+				} )
+				.columns.adjust()
+				.responsive.recalc();
+		} );
+	</script>
+    </script>
 
 
 </body>
